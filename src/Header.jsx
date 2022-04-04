c91472a5e9ec130858103fbd591d7ef86fb2309d
ashfaque khan <ashfaquek036@gmail.com>
2022-04-04 10:38:12 +0530

    navbar completed

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,46 @@
 import React from "react";
-import HomeIcon from "@mui/icons-material/Home";
-import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
 import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
+import "./header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>i am header</h1>
-      <HomeIcon />
-      <AccessAlarmIcon />
-      <SearchIcon />
+    <div className="header">
+      <div className="header__icons">
+        <div className="header__icon header__icon--active">
+          <HomeIcon />
+          <p>Home</p>
+        </div>
+
+        <div className="header__icon">
+          <FlashOnIcon />
+          <p>Trending</p>
+        </div>
+        <div className="header__icon">
+          <LiveTvIcon />
+          <p>Verified</p>
+        </div>
+        <div className="header__icon">
+          <VideoLibraryIcon />
+          <p>Collection</p>
+        </div>
+        <div className="header__icon">
+          <SearchIcon />
+          <p>Search</p>
+        </div>
+        <div className="header__icon">
+          <PersonOutlineIcon />
+          <p>Account</p>
+        </div>
+      </div>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Hulu_Logo.svg/800px-Hulu_Logo.svg.png"
+        alt="logo"
+      />
     </div>
   );
 };
