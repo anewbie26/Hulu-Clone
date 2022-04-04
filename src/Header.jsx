a636f4eac5b42ace3be54deca7d26a19a028b995
ashfaque khan <ashfaquek036@gmail.com>
2022-04-04 09:07:04 +0530

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+
+
+const Header = () => {
+  return (
+    <div>
+      <h1>i am header</h1>    
+      <HomeIcon />
+      <AccessAlarmIcon />
+    </div>
+  )
+}
+
+export default Header
