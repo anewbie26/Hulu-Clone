453bcb9533c9bb69825562b51c12803541ce596e
ashfaque khan <ashfaquek036@gmail.com>
2022-04-04 08:29:03 +0530

    new commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import HomeIcon from '@mui/icons-material/Home';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-
+import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Header = () => {
   return (
     <div>
-      <h1>i am header</h1>    
+      <h1>i am header</h1>
       <HomeIcon />
       <AccessAlarmIcon />
+      <SearchIcon />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
